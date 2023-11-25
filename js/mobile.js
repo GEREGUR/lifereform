3882e1f2ba539b193d5dfec10ac7a03a4b56464d
GEREGUR <georgerubaylo@gmail.com>
2023-11-25 15:23:42 +0300

    HI

---
+++ js/mobile.js
@@ -0,0 +1,8 @@
+// const phoneBtn = document.querySelector('.mobile-phone')
+
+// document.addEventListener('scroll', (e) => {
+//     const visibility = primaryNav.getAttribute('data-visible');
+//     if (visibility === true && ) {
+
+//     }
+// })
---
+++ js/nav.js
@@ -0,0 +1,18 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    if (visibility === "false"){
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true)
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+}); 
+
+const phoneNumber = document.querySelector(".mobile-phone");
+phoneNumber.addEventListener("click", () => {
+    window.open(`tel:${89151693838}`, '_self');
+})
